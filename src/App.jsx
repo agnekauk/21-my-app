7ddf50d05d831edfe55ec5dc79dfd06fd54097c5
agnekauk <agnenemeikaite@gmail.com>
2022-07-03 18:08:34 +0300

    Update (with reducer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,60 @@
 import './CSS/App.css';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import ReactPaginate from 'react-paginate';
+import React, { useState, useEffect, useReducer } from 'react';
+// import ReactPaginate from 'react-paginate';
 import Countries from './Components/Countries';
-import AllCountriesButton from './Components/AllCountriesButton';
 import Button from './Components/Button';
+import countriesReducer from './Reducers/countriesReducer';
+import { getFromServer, sortByNameAsc, sortByNameDesc, filterSmallerLTU, filterFromOceania, showAll } from './Actions/countriesActions';
 
 const App = () => {
-    const [countries, setCountries] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [countriesPerPage] = useState(10);
+    const [countries, dispachCountries] = useReducer(countriesReducer, []);
     const [AtoZ, setAtoZ] = useState(true);
 
-    useEffect(() => {
-        const fetchCountries = async () => {
-            setLoading(true);
-            const res = await axios.get('https://restcountries.com/v2/all?fields=name,region,area');
-            setCountries(res.data);
-            setLoading(false);
-        }
+    
+    // const [currentPage, setCurrentPage] = useState(0);
+    // const [countriesPerPage] = useState(10);
 
-        fetchCountries();
+    useEffect(()=> {
+        axios.get('https://restcountries.com/v2/all?fields=name,region,area')
+        .then(res => {
+            dispachCountries(getFromServer(res.data));
+        });
     }, []);
 
-    const sort = () => {
-        let sorted = [];
+     const sort = () => {
         if (AtoZ) {
-            sorted = [...countries].sort((a, b) => (a.name > b.name) ? 1 : -1);
-            setAtoZ(false);
-        } else {sorted = [...countries].sort((a, b) => (b.name > a.name) ? 1 : -1)
-            setAtoZ(true)};
-        setCountries(sorted);
-    };
-    
-    const filterSmallerThanLT = () => {
-        const smaller = [...countries.filter(country=> country.area < 65300)];
-        setCountries(smaller);
-    };
+            dispachCountries(sortByNameAsc());
+            setAtoZ(x => !x);
+        } else {
+            dispachCountries(sortByNameDesc());
+            setAtoZ(x => !x);
+        }
+        
+     }
 
-    const filterOceania = () => {
-        const oceania = [...countries.filter(country=> country.region ==='Oceania')];
-        setCountries(oceania);
-    };
+    // function handlePageClick({ selected:selectedPage}) {
+    //     setCurrentPage(selectedPage);
+    // }
 
-    function handlePageClick({ selected:selectedPage}) {
-        setCurrentPage(selectedPage);
-    }
+    // const offset= currentPage * countriesPerPage;
 
-    const offset= currentPage * countriesPerPage;
+    // const currentCountries = countries
+    //     .slice(offset, offset+countriesPerPage);
 
-    const currentCountries = countries
-        .slice(offset, offset+countriesPerPage);
-
-    const pageCount = Math.ceil(countries.length/countriesPerPage);
+    // const pageCount = Math.ceil(countries.length/countriesPerPage);
 
     return (
         <div className='container'>
-            <AllCountriesButton></AllCountriesButton>
+            <h1 className='title' onClick={() => dispachCountries(showAll())}>List of the countries</h1>
             <div className='navigation'>
-                <Button buttonName='Smaller than Lithuania' class='btn btn-primary' do={filterSmallerThanLT}></Button>
-                <Button buttonName='All from Oceania' class='btn btn-primary' do={filterOceania}></Button>
+                <Button buttonName='Smaller than Lithuania' class='btn btn-primary' do={() => dispachCountries(filterSmallerLTU())}></Button>
+                <Button buttonName='All from Oceania' class='btn btn-primary' do={() => dispachCountries(filterFromOceania())}></Button>
                 <Button class='btn btn-secondary' imgSrc={require('./img/az.png')} alt='az' do={sort}></Button>
             </div>
-            <Countries countries={currentCountries} loading={loading}></Countries>
-            <ReactPaginate
+            <Countries countries={countries}></Countries> 
+            {/* buvo currentCountries */}
+            {/* <ReactPaginate
                 previousLabel={'Previous'}
                 nextLabel = {'Next'}
                 pageCount = {pageCount}
@@ -74,7 +64,7 @@ const App = () => {
                 nextLinkClassName = {'pagination__link'}
                 disabledClassName = {'pagination__link--disabled'}
                 activeClassName = {'pagination__link--active'}>
-                </ReactPaginate>
+                </ReactPaginate> */}
         </div>
     );
 };
